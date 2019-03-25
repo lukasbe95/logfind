797ae0fd531180cb18744bd14895d2e470573cbd
lukasbe95 <lukasbe@gazeta.pl>
2019-03-25 14:40:38 +0100

    Nice way to copy argv table (2d pointer and memcpy).

--- logfind.c
+++ logfind.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
 #include "dbg.h"
 
 #define DEFAULT_FILE "/home/lukas/logfind"
 
-int print_whole_file(char path[], char filename[]);
+int search_in_file(char path[], char filename[], int only_print);
 
 int
 list_directory(char path[])
@@ -18,8 +19,8 @@ list_directory(char path[])
     {
         while ((dir = readdir(d)) != NULL)
         {
-            printf("%s\n", dir->d_name);
-            print_whole_file(path, dir->d_name);
+            // printf("%s\n", dir->d_name);
+            search_in_file(path, dir->d_name, 0);
         }
         closedir(d);
 
@@ -27,7 +28,16 @@ list_directory(char path[])
     return 0;
 }
 int
-print_whole_file(char path[], char filename[])
+check_argument(char *line, char arg[])
+{
+    if(strstr(line, arg) != NULL)
+    {
+        return 0;
+    }
+    return -1;
+}
+int
+search_in_file(char path[], char filename[], int only_print)
 {
     char whole_path[256];
     FILE *file;
@@ -35,13 +45,17 @@ print_whole_file(char path[], char filename[])
     size_t len = 0;
     ssize_t read;
     strcpy(whole_path, path);
-    strcat(whole_path,filename);
+    strcat(whole_path, filename);
     file = fopen(whole_path, "r");
     if (file != NULL)
     {
         printf("FILENAME: %s\n\n", whole_path);
-        while ((read = getline(&line, &len, file)) != -1) {
-            printf("%s", line);
+        while ((read = getline(&line, &len, file)) != -1)
+        {
+            if(check_argument(line, "dupa") == 0)
+            {
+                printf("%s", line);
+            }
         }
     }
     fclose(file);
@@ -70,13 +84,21 @@ is_or(int array_size, char *arguments[])
 }
 int main(int argc, char *argv[]){
     check(argc > 1, "There is nothing to find.");
-    // for(int i = 0; i < argc; i++){
-    //   printf("%s\n", argv[i]);
-    // }
+    char **key_words = malloc((argc)*sizeof(*key_words));
+    for(int i = 1; i < argc; i++)
+    {
+        size_t length = strlen(argv[i]) + 1;
+        key_words[i-1] = malloc(length);
+        memcpy(key_words[i-1], argv[i], length);
+    }
+    for (int j = 0; j < argc - 1; j++) //new array is smaller, so must be argc-1
+    {
+        printf("%s\n", key_words[j]);
+    }
     // printf("%d\n", check_logfile());
-    check(check_logfile(), "Please create logfile in home directory.")
+    // check(check_logfile(), "Please create logfile in home directory.")
     // printf("%d\n", is_or(argc, argv));
-    list_directory("/home/lukas/");
+    // list_directory("/home/lukas/");
     return 0;
     error:
     return -1;
